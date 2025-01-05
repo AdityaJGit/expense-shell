96b7a1dcc20b61ea510ca2c5f2083197e4c34dde
adityajatla <adityajatlaofficial@gmail.com>
2025-01-05 10:23:18 +0530

    Expense-Shell

--- backend.sh
+++ backend.sh
@@ -67,7 +67,7 @@ VALIDATE $? "unzip backend"
 npm install &>>$LOG_FILE_NAME
 VALIDATE $? "Installing Dependencies"
 
-cp /home/root/expense-shell/backend.service/etc/systemd/system/backend.service
+cp backend.service/etc/systemd/system/backend.service
 
 #Prepare Mysql Schema
 
